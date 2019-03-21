bb29a5c28e9d2b470a8925956bd5a36872461ae6
unknown <awatson23>
2019-03-20 20:16:05 -0400

    pushing changes

--- js/main.js
+++ js/main.js
@@ -1,23 +1,47 @@
 (() => {
 	console.log('fired');
 
-	let power = document.querySelectorAll('#power');
+	let powerB = document.querySelectorAll('#powerBeat');
+	let powerE = document.querySelectorAll('#powerEffect');
+	let powerM = document.querySelectorAll('#powerMelody');
+	let powerIcon = document.querySelectorAll('#power')
 
+	let roboBeat = document.querySelectorAll('#robotOne');
+	let roboEffect = document.querySelectorAll('#robotTwo');
+	let roboMelody = document.querySelectorAll('#robotThree')
+	let roboZone = document.querySelectorAll('#robots')
 
 	function powerDrop() {
-		
-		initDrag();
 
-		powerBeat.querySelectorAll('#power').forEach(img => {
+
+		powerIcon.querySelectorAll('#power').forEach(img => {
 			img.addEventListener("dragstart", function(e) {
 			});
 		});
+
+		initDrag();
+
+	}
+
+	function initDrag() {
+
+		roboZones.forEach(zone => {
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log('dragged over me!');
+
+		});
+
+
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log('you dropped somethin on me');
+
 
 		currentTrack = this.dataset.currenttrack;
 
 		aud.src = `music/beat/${currentTrack}`;
 		aud.load();
 		aud.play();
-	}
-
-	})();
+	});
+})();
