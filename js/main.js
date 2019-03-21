3debeeab6209567b2ff40e2ce138d424ef3e4a56
unknown <awatson23>
2019-03-21 12:08:36 -0400

    JS problems fixed with trevor

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
 (() => {
 	console.log('fired');
 
-	let power = document.querySelectorAll('#power');
+	let power = document.querySelectorAll('img'),
+		robos = document.querySelectorAll('.robos'),
+		sound = document.querySelectorAll('.sound');
 
 
-	function powerDrop() {
-		
-		initDrag();
 
-		powerBeat.querySelectorAll('#power').forEach(img => {
+	function initDrag() {
+
+		power.forEach(img => {
 			img.addEventListener("dragstart", function(e) {
+				console.log('dragging');
 			});
 		});
 
+	}
+
+	robos.forEach(zone => {
+		zone.addEventListener('dragover', function(e) {
+			e.preventDefault();
+			console.log('dragged');
+		});
+
+		zone.addEventListener('drop', function(e){
+			//e.preventDefault();
+			console.log('dropped')
+		});
+	});
+		
+	function playAudio() {
 		currentTrack = this.dataset.currenttrack;
 
 		aud.src = `music/beat/${currentTrack}`;
 		aud.load();
 		aud.play();
-	}
 
-	})();
+		};
+
+
+	initDrag();
+
+})();
